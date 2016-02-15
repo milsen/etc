095e077bdc20ecbbe83f7bb01b933af2318db1c8
milsen <milsen@users.noreply.github.com>
2016-02-15 18:58:52 +0100

    Fix broken .profile-symlink in install.sh

--- install.sh
+++ install.sh
@@ -34,7 +34,7 @@ ln -sfv "$DOTFILES_DIR/shell/bash/.bash_aliases"   "$HOME"
 ln -sfv "$DOTFILES_DIR/shell/bash/.bash_functions" "$HOME"
 ln -sfv "$DOTFILES_DIR/shell/bash/.bash_logout"    "$HOME"
 ln -sfv "$DOTFILES_DIR/shell/bash/.bashrc"         "$HOME"
-ln -sfv "$DOTFILES_DIR/bash/.profile"              "$HOME"
+ln -sfv "$DOTFILES_DIR/shell/.profile"             "$HOME"
 ln -sfv "$DOTFILES_DIR/coreutils/"                 "$HOME"/.config/
 ln -sfv "$DOTFILES_DIR/git/"                       "$HOME"/.config/
 ln -sfv "$DOTFILES_DIR/misc_runcom/.eclimrc"       "$HOME"
