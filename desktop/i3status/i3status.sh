b547c3e702b88c4c661a4f9f7831660518d5fe26
milsen <milsen@users.noreply.github.com>
2020-09-17 01:11:59 +0200

    Add i3 config

---
+++ desktop/i3status/i3status.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# Adds the window title to the i3status.
+
+num_title_cols=100
+
+i3status | while :
+do
+    read line
+    id="$(xprop -root | awk '/_NET_ACTIVE_WINDOW\(WINDOW\)/{print $NF}')"
+    name="$(xprop -id $id | awk '/_NET_WM_NAME/{$1=$2="";print}' | cut -d'"' -f2)"
+    name_length="${#name}"
+
+    if [[ "$name_length" -lt "$num_title_cols" ]]; then
+      padding_length=$(("$num_title_cols" - "$name_length"))
+      padding="$(printf %"$padding_length"s)"
+      echo "$name$padding $line" || exit 1
+    else
+      shortened_name="$(echo "$name" | head -c "$(("$num_title_cols" - 4))")"
+      echo "$shortened_name...  $line" || exit 1
+    fi
+done
